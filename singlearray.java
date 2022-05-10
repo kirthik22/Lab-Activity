307fa452c23be1a87439ef17b0bd3784a11a2d72
Kirthik <104076913+kirthik22@users.noreply.github.com>
2022-05-10 22:03:53 +0530

    Add files via upload

---
+++ singlearray.java
@@ -0,0 +1,12 @@
+class singlearray
+{
+public static void main(String args[])
+{
+int a[]=new int[3];
+a[0]=10;
+a[1]=120;
+a[2]=19;
+for(int i=0;i<a.length;i++)
+System.out.println(a[i]);
+}
+}
---
+++ singlesumarray.java
@@ -0,0 +1,13 @@
+class singlesumarray
+{
+public static void main(String args[])
+{
+int a[]={1,33,55};
+int sum=0;
+for(int i=0;i<a.length;i++)
+{
+sum=sum+a[i];
+}
+System.out.println(sum);
+}
+}
